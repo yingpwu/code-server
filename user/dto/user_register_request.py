9e879a7cf716bf60b43a6f081dc553df91b0125e
coder <coder@qq.com>
2022-07-28 18:17:44 +0800

    feat: 用户注册dto创建

--- user/dto/user_register_request.py
+++ user/dto/user_register_request.py
@@ -1,4 +1,9 @@
 class UserRegisterRequest():
     def __init__(self, username, password):
-        self.username=username
-        self.password=password
+        self.username = username
+        self.password = password
+
+    def __eq__(self, other):
+        if not isinstance(other, UserRegisterRequest):
+            return False
+        return self.username == other.username and self.password == other.password
