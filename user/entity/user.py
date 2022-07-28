0a0e39b660e4e4da14b54cdb44b914824e7c79d0
coder <coder@qq.com>
2022-07-28 09:28:24 +0800

    feat: 新增用户实体

---
+++ user/entity/user.py
@@ -0,0 +1,9 @@
+class User():
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+
+    def __eq__(self, other):
+        if not isinstance(other, User):
+            return False
+        return self.username ==other.username
