f4e8046a563925c380739a8aa933c00796481ba8
coder <coder@qq.com>
2022-07-30 16:53:39 +0800

    feat: 用户实体增加token的编码

---
+++ user/utils/jwt.py
@@ -0,0 +1,13 @@
+import jwt
+
+
+import jwt
+
+
+def encode(payload):
+    return jwt.encode(payload, 'screct', algorithm='HS256')
+
+
+def decode(payload):
+    return jwt.decode(payload, 'screct', algorithm='HS256')
+
